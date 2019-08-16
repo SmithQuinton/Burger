781cef7504621bbab4ad93be4f202552da5cfa87
Quinton Smith <smith.quinton42@gmail.com>
2019-08-16 15:03:10 -0400

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+    VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name)
+    VALUES ("Roadhouse Burger");
+INSERT INTO burgers (burger_name)
+    VALUES ("Chilli Cheeseburger");
